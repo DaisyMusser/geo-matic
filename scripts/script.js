269bc74319143eb2a3580312274b57f7532c43c7
Quattro Musser <quattromusser@gmail.com>
2024-02-28 20:14:32 -0700

    Just need to display the updated geometery in a nice way... then gtg.

--- scripts/script.js
+++ scripts/script.js
@@ -13,12 +13,14 @@ function to_degrees(radians) {
 }
 
 function getNewGeo() {
-    let WB = 1199;
-    let HT = 63.5;
-    let AT = 10;
+
+    let WB = document.getElementById("WB").value;
+    let HT = document.getElementById("HT").value;
+    let AT = document.getElementById("AT").value;
     let y = to_radians(180 - HT);
     let nWB = Math.sqrt(WB**2 + AT**2 - 2*WB*AT*Math.cos(y))
-    let nHT = to_degrees(Math.acos((nWB**2 + AT**2 - WB**2)/(2*nWB*AT)))
+    let nHT = to_degrees(Math.acos((nWB**2 + AT*10*2 - WB**2)/(2*nWB*AT)))
+    console.log(nHT);
 }
 
 
